3593670a9f4bb2a180635d378d75da391a339611
andrey.nascimento4@etec.sp.gov.br <e107ti@etec.sp.gov.br>
2023-05-24 17:52:49 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+    double glicemia;
+
+    Console.Clear();
+
+    Console.WriteLine("*** Diabetes Gestacional ***");
+    Console.WriteLine();
+    
+    Console.WriteLine("Digite o valor da glicemia em mg/dL....: ");
+    glicemia = double.Parse(Console.ReadLine()!);
+
+    if (glicemia < 92)
+    {
+        Console.WriteLine($"Nível de glicemia....: {glicemia}");
+        Console.WriteLine("Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
+    }
+    if (glicemia >= 92 && glicemia < 126)
+    {
+        Console.WriteLine($"Nível de glicemia....: {glicemia}");
+        Console.WriteLine("Compátivel com Diabetes Gestacional");
+    }
+    if (glicemia >= 126)
+    {
+        Console.WriteLine($"Nível de glicemia....: {glicemia}");
+        Console.WriteLine("Diavetes de Mellitus na Gravidez");
+    }
